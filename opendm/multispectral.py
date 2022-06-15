c65a8d5242f9bb024a8c231b9908121d3deaf62d
usplm <admin@usplm.net>
2022-06-15 03:44:51 -0400

    Reduced gaussFiltSize to default 5

--- opendm/multispectral.py
+++ opendm/multispectral.py
@@ -458,7 +458,7 @@ def find_ecc_homography(image_gray, align_image_gray, number_of_iterations=1000,
 
         try:
             log.ODM_INFO("Computing ECC pyramid level %s" % level)
-            _, warp_matrix = cv2.findTransformECC(ig, aig, warp_matrix, cv2.MOTION_HOMOGRAPHY, criteria, inputMask=None, gaussFiltSize=9)
+            _, warp_matrix = cv2.findTransformECC(ig, aig, warp_matrix, cv2.MOTION_HOMOGRAPHY, criteria, inputMask=None, gaussFiltSize=5)
         except Exception as e:
             if level != pyramid_levels:
                 log.ODM_INFO("Could not compute ECC warp_matrix at pyramid level %s, resetting matrix" % level)
