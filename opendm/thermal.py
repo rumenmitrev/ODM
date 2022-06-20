8d6497c732df55e6bf4badf38ce4609c63a82ac8
usplm <admin@usplm.net>
2022-06-20 07:20:35 -0400

    Apply LANCZOS4 interpolation when resizing thermal images

--- opendm/thermal.py
+++ opendm/thermal.py
@@ -14,7 +14,8 @@ def resize_to_match(image, match_photo = None):
         if w != match_photo.width or h != match_photo.height:
             image = cv2.resize(image, None, 
                     fx=match_photo.width/w, 
-                    fy=match_photo.height/h)
+                    fy=match_photo.height/h,
+                    interpolation=cv2.INTER_LANCZOS4)
     return image
 
 def dn_to_temperature(photo, image, dataset_tree):
